7de0800b9ec6c837ae14cda8565f69ca7dc78e2b
isel-har <ismailhrb1337@gmail.com>
2024-10-13 21:57:51 +0100

    finish crud operations

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,27 +1,64 @@
 import { Link, useParams } from 'react-router-dom';
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { API_URL } from './NotesListPage';
 
 function NotePage() {
 
-    const textRef = useRef(null);
-    let { id }    = useParams();
+    let { id } = useParams();
     const [note, setNote] = useState({});
-    let isNew = (id === undefined || !Number.isInteger(id)) ? true : false;
-
+    
     const getNote = async() => {
-        const response = await fetch(`${API_URL}/${id}`);
-        const data  = await response.json();
-        setNote(data.data);
+        const response = await fetch(`${API_URL}/note/${id}`);
+        if (await response.status === 200)
+        {
+            let data = await response.json();
+            setNote(data.data);
+        }
+    }
+    useEffect(() => {
+        if (id === undefined)
+            return ;
+        getNote();
+    }, []);
+
+    const createNote = async () => {
+
+        await fetch(`${API_URL}/create`, {
+            headers: {"Content-Type": "application/json"},
+            method: 'POST',
+            body: JSON.stringify(note)
+        })
+    }
+    const deleteNote = async () => {
+ 
+        await fetch(`${API_URL}/note/${id}`, {
+            method: 'DELETE',
+        });
     }
 
+    const updateNote = async() => {
 
+        await fetch(`${API_URL}/note/${id}`, {
+            headers: {"Content-type" : "application/json"},
+            method: 'PUT',
+            body: JSON.stringify(note)
+        })
+    }
+
+    const submitHandler = () => {
+
+        if (id === undefined && note.body) // more parsing
+            createNote();
+        else if (id !== undefined && !note.body)
+            deleteNote();
+        else if (id !== undefined)
+            updateNote();
+    }
     return (
         <div className="paper">
-            <Link to='/' className='back'></Link>
-            <button className="post" onClick={postNote}></button>
-            <Link to='/' className="discard"></Link>
-            <textarea ref={textRef} className="Typing" maxLength={500}>{isNew & (note) || ""}</textarea>
+            <Link to='/' className='back' onClick={submitHandler}></Link>
+            <Link to='/' className="discard" onClick={deleteNote}></Link>
+            <textarea className="Typing" onChange={(e) => {setNote({title:"mn 3ndi", body:e.target.value})}} value={note?.body} maxLength={500} />
         </div>
     );
 }
