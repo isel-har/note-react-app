5cb1b7112436e6b0c5eab2a30901b4e4ab3760e3
isel-har <115952288+isel-har@users.noreply.github.com>
2024-10-14 17:43:20 +0100

    Update Note.js
    read from note object

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 function Note({note})
 {   
-    const timestamp = (updated_at) => {
-        let date = new Date(updated_at)
+    const timestamp = () => {
+        let date = new Date(note.updated_at)
         return date.toLocaleDateString();
     }
 
@@ -12,7 +12,7 @@ function Note({note})
     <Link to={`/note/${note.id}`}>
         <div className="Note">
             <h3 className="title">{note.title}</h3>
-            <p className="body">{ timestamp(note.updated_at) } :  {note.body}</p>
+            <p className="body">{ timestamp() } :  {note.body}</p>
             <span className="edit-symbol"></span>
         </div>
     </Link>);
